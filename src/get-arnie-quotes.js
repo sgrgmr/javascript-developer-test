2c4389019c0a5ef5a13886c87eda575dc522d402
Sagar Ghimire <gmrsagar+gh@gmail.com>
2025-10-02 17:58:33 +1000

    feat: implement getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,24 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+const generateResult = (response) => {
+  try {
+    const { message } = JSON.parse(response.body);
+
+    if (response.status === 200) {
+      return { "Arnie Quote": message };
+    }
+
+    return { FAILURE: message };
+  } catch (err) {
+    return { FAILURE: err?.message };
+  }
+};
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const requests = urls.map(httpGet);
+  const responses = await Promise.all(requests);
+
+  return responses.map(generateResult);
 };
 
 module.exports = {
